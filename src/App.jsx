17a5b5aa0b84dec30e8fe4f4e2f9011cb036c7a8
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2024-04-22 17:51:31 +0200

    installing react-router-dom and implementing the routes and the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound";
 const App = () => {
-  const asd = 2;
-  return <div>WorldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
