a49ca90308bee476043af60f4edba24755515e53
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2024-04-22 21:49:26 +0200

    adding navigation

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Home from "./pages/Home";
 import PageNotFound from "./pages/PageNotFound";
+import PageNav from "./components/PageNav";
 const App = () => {
   return (
     <BrowserRouter>
+      <PageNav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="product" element={<Product />} />
