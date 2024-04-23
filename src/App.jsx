3523f792a446ee071e0fd3f91b3dd1bf90d84b57
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2024-04-23 21:38:12 +0200

    implementing the cities

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,53 @@ import PageNotFound from "./pages/PageNotFound";
 import PageNav from "./components/PageNav/PageNav";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList/CityList";
+import { useEffect, useState } from "react";
+const initialCities = [
+  {
+    cityName: "Lisbon",
+    country: "Portugal",
+    emoji: "🇵🇹",
+    date: "2027-10-31T15:59:59.138Z",
+    notes: "My favorite city so far!",
+    position: {
+      lat: 38.727881642324164,
+      lng: -9.140900099907554,
+    },
+    id: 73930385,
+  },
+  {
+    cityName: "Madrid",
+    country: "Spain",
+    emoji: "🇪🇸",
+    date: "2027-07-15T08:22:53.976Z",
+    notes: "",
+    position: {
+      lat: 40.46635901755316,
+      lng: -3.7133789062500004,
+    },
+    id: 17806751,
+  },
+  {
+    cityName: "Berlin",
+    country: "Germany",
+    emoji: "🇩🇪",
+    date: "2027-02-12T09:24:11.863Z",
+    notes: "Amazing 😃",
+    position: {
+      lat: 52.53586782505711,
+      lng: 13.376933665713324,
+    },
+    id: 98443197,
+  },
+];
 const App = () => {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setCities(initialCities);
+    setIsLoading(false);
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
@@ -15,8 +61,14 @@ const App = () => {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>List of Cities</p>} />
-          <Route path="cities" element={<p>List of Cities</p>} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="countries" element={<p>List of Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
