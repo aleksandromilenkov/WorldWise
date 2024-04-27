e8c9dcc6d7c65d0de8ee0c385e7b1c3fb074259e
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2024-04-27 20:36:08 +0200

    remembering the position of the map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Map.module.css";
 import { useNavigate, useSearchParams } from "react-router-dom";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import { useCities } from "../../contexts/CitiesContext";
+import { map } from "leaflet";
 const Map = () => {
   const navigate = useNavigate();
   const { cities } = useCities();
@@ -10,6 +11,11 @@ const Map = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
+  useEffect(() => {
+    if (lat && lng) {
+      setMapPosition([lat, lng]);
+    }
+  }, [lat, lng]);
   return (
     <div
       className={styles.mapContainer}
@@ -35,9 +41,16 @@ const Map = () => {
             </Popup>
           </Marker>
         ))}
+        <ChangeCenter position={mapPosition} />
       </MapContainer>
     </div>
   );
+};
+
+const ChangeCenter = ({ position }) => {
+  const map = useMap();
+  map.setView(position);
+  return null;
 };
 
 export default Map;
